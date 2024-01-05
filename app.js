b48946d859c6455dcf2fa39d70aedfff1bb35d23
Jessica Kelly <JessKelly91@outlook.com>
2024-01-05 12:33:58 -0500

    adding /all route

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-// try
-// catch (e){
-//     next(e)
-// }
-
 app.get('/mean', (req, res, next) => {
     try{
         if(!req.query.nums){
@@ -27,18 +22,18 @@ app.get('/mean', (req, res, next) => {
 
         const srtdNums = sortArr(nums);
     
-        const avg = calculateAverage(srtdNums);
+        const mean = calculateAverage(srtdNums);
     
-        resp = {
+        const resp = {
             operation: "mean",
-            value: avg
+            value: mean
         };
     
         return res.json(resp)
     } catch (e) {
         next(e)
     }
-})
+});
 
 app.get('/median', (req, res, next) =>{
     try{
@@ -59,7 +54,7 @@ app.get('/median', (req, res, next) =>{
     
         const med = calculateMedian(srtdNums);
         
-        resp = {
+        const resp = {
             operation: "median",
             value: med
         }
@@ -69,7 +64,7 @@ app.get('/median', (req, res, next) =>{
         next(e)
     }
 
-})
+});
 
 app.get('/mode', (req, res, next) =>{
     try{
@@ -88,22 +83,56 @@ app.get('/mode', (req, res, next) =>{
         
         const mode = calculateMode(nums);
     
-        resp = {
+        const resp = {
             operation: "mode",
             value: mode
         }
     
         return res.json(resp)
     } catch (e){
-        next(e)
+        next(e);
     }
 
+});
+
+app.get('/all', (req, res, next) => {
+    try{
+        if(!req.query.nums){
+            throw new ExpressError("Numbers are required", 400);
+        }
+
+        const numsString = req.query.nums;
+        const nums = createNumsArr(numsString);
+
+        for(let i=0; i < nums.length; i++){
+            if(isNaN(nums[i])){
+                throw new ExpressError("Must include only numbers", 400);
+            }
+        }
+
+        const srtdNums = sortArr(nums);
+        const mean = calculateAverage(srtdNums);
+        const med = calculateMedian(srtdNums);
+        const mode = calculateMode(srtdNums);
+
+        const resp = {
+            operation: "all",
+            mean: mean,
+            median: med,
+            mode: mode
+        }
+
+        return res.json(resp);
+    }
+    catch(e){
+        next(e);
+    }
 })
 
 app.use( (req, res, next) => {
     const e = new ExpressError("Page not found", 404);
     next(e);
-})
+});
 
 app.use( (err, req, res, next) => {
     let status = err.status || 500;
@@ -112,8 +141,8 @@ app.use( (err, req, res, next) => {
     return res.status(status).json({
         error: {msg, status}    
     });
-})
+});
 
 app.listen(3000, () =>{
     console.log('App open on port 3000');
-})
+});
